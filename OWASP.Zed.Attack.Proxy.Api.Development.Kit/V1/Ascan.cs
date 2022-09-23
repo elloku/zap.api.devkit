68555763a00c982637a6e50de4b524887a586bc5
songxuanlong <songxuanlong@mediinfo.cn>
2022-09-23 17:33:21 +0800

    add Ascan.cs

---
+++ OWASP.Zed.Attack.Proxy.Api.Development.Kit/V1/Ascan.cs
@@ -0,0 +1,71 @@
+namespace OWASP.Zed.Attack.Proxy.Api.Development.Kit.V1;
+
+public class Ascan
+{
+    private const string AscanViewStatus = "/ascan/view/status/";
+    private const string AscanViewScanProgress = "/ascan/view/scanProgress/";
+    private const string AscanViewMessagesIds = "";
+    private const string ascanViewAlertsIds = "";
+    private const string ascanViewScans = "";
+    private const string ascanViewScanPolicyNames = "";
+    private const string ascanViewExcludedFromScan = "";
+    private const string  ascanViewScanners= "";
+    private const string  ascanViewPolicies= "";
+    private const string ascanViewAttackModeQueue = "";
+    private const string  ascanViewExcludedParams= "";
+    private const string  ascanViewOptionExcludedParamList= "";
+    private const string  ascanViewExcludedParamTypes= "";
+    private const string  ascanViewOptionAttackPolicy= "";
+    private const string  ascanViewOptionDefaultPolicy= "";
+    private const string  ascanViewOptionDelayInMs= "";
+    private const string  ascanViewOptionHandleAntiCSRFTokens= "";
+    private const string  ascanViewOptionHostPerScan= "";
+    private const string  ascanViewOptionMaxChartTimeInMins= "";
+    private const string ascanViewOptionMaxResultsToList = "";
+    private const string ascanViewOptionMaxRuleDurationInMins = "";
+    private const string ascanViewOptionMaxScanDurationInMins = "";
+    private const string ascanViewOptionMaxScansInUI = "";
+    private const string ascanViewOptionTargetParamsEnabledRPC = "";
+    private const string  ascanViewOptionTargetParamsInjectable= "";
+    private const string  ascanViewOptionThreadPerHost= "";
+    private const string  ascanViewOptionAddQueryParam= "";
+    private const string  ascanViewOptionAllowAttackOnStart= "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+    private const string  = "";
+        
+    
+    
+}
